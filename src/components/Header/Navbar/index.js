4fac57a98f2803805678e212dc6a9cbf41560314
cuong_dam <cuongdm@rikkeisoft.com>
2022-04-08 15:42:39 +0700

    add login.js back to index.js

--- src/components/Header/Navbar/index.js
+++ src/components/Header/Navbar/index.js
@@ -13,7 +13,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Banner from "../images/left-banner.jpg";
 
-import Login from "./Login";
+// import Login from "./Login";
+// import React, { useState } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import Modal from "react-bootstrap/Modal";
+
+import { defaultLogin } from "../utils";
+
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(8).max(32).required(),
+});
+
 
 const Navbar = () => {
 
@@ -24,6 +37,120 @@ const Navbar = () => {
   };
   const handleClose = () => {
     setShow(false);
+  };
+
+  const Login = (props) => {
+
+    // const { handleClose, show } = props;
+  
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+      reset
+    } = useForm({ resolver: yupResolver(schema), });
+  
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
+    const isLogin = (data) => {
+      if (
+        defaultLogin.defaultUsername === data.email &&
+        defaultLogin.defaultPassword === data.password
+      ) {
+        return true;
+      } else return false;
+    };
+    
+  const onSubmitHandler = (data) => {
+    console.log({data});
+    isLogin(data);
+  }
+  
+    const handleCloseModal = () => {
+      reset();
+      handleClose();
+    };
+  
+    return (
+      <>
+        <Modal show={show} onHide={handleCloseModal}>
+          <Modal.Header style={{ backgroundColor: "orange" }} closeButton>
+            <Modal.Title style={{ marginLeft: "200px", color: "white" }}>
+              Sign In
+            </Modal.Title>
+          </Modal.Header>
+  
+          <Modal.Body>
+            <form onSubmit={handleSubmit(onSubmitHandler)}>
+              <label className="form-label"> Enter your email: </label>
+              <input
+                // value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                style={{ display: "flex", width: "100%", marginBottom: "10px" }}
+                {...register("email")}
+                placeholder="your email"
+                required
+              />
+              {errors.email && (
+                <span
+                  style={{
+                    color: "red",
+                    textDecoration: "underline red",
+                    marginBottom: "15px",
+                    display: "flex",
+                  }}
+                >
+                  email không hợp lệ.
+                </span>
+              )}
+  
+              <label className="form-label"> Your Password: </label>
+              <input
+                // value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                style={{ display: "flex", width: "100%" }}
+                {...register("password")}
+                placeholder="your password"
+                required
+              />
+              {errors.password && (
+                <span style={{ color: "red", textDecoration: "underline red" }}>
+                  Password must be at least 8 characters.
+                </span>
+              )}
+  
+              <hr />
+              <div style={{ display: "flex", justifyContent: "right" }}>
+                <button
+                  onClick={handleCloseModal}
+                  type="close"
+                  style={{
+                    marginRight: "29px",
+                    backgroundColor: "#ccc",
+                    width: "65px",
+                    height: "35px",
+                  }}
+                >
+                  Close
+                </button>
+                <button
+                  onClick={handleSubmit(onSubmitHandler)}
+                  type="submit"
+                  style={{
+                    backgroundColor: "orange",
+                    width: "65px",
+                    height: "35px",
+                  }}
+                >
+                  Signin
+                </button>
+              </div>
+            </form>
+          </Modal.Body>
+        </Modal>
+      </>
+    );
   };
 
   return (
@@ -68,12 +195,13 @@ const Navbar = () => {
           {/* <NavLink to="/sign-up">Sign Up</NavLink> */}
         </NavMenu>
         <NavBtn>
-          <NavBtnLink to="/log-in"  onClick={handleShow}>
+          <NavBtnLink to="/login"  onClick={handleShow}>
             Log In
           </NavBtnLink>
         </NavBtn>
         
-        <Login show={show} handleClose={handleClose} />
+        <Login />
+        {/* <Login show={show} handleClose={handleClose} /> */}
 
         <div>
           <img className="banner-img" src={Banner} />
